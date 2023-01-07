d5e742a76a88c381d7e16eea82c09b3c19ea58f1
Yannik <yannik.braendle@t-online.de>
2023-01-07 15:32:50 +0100

    shipcontroller upgrade

--- Orbital Oranges/Assets/_Scripts/Player/ShipController.cs
+++ Orbital Oranges/Assets/_Scripts/Player/ShipController.cs
@@ -1,18 +1,73 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.InputSystem;
 public class ShipController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+
+    private bool _isControlled;
+    private Vector3 _input;
+
+
+    public void SetControlled(bool isControlled)
     {
-        
+        _isControlled = isControlled;
+
     }
 
-    // Update is called once per frame
-    void Update()
+    public void OnMoveInput(InputAction.CallbackContext context)
     {
-        
+        if (_isControlled)
+        {
+            Vector2 input = context.ReadValue<Vector2>();
+            _input = new Vector3(input.x, _input.z, input.y);
+            Debug.Log(input);
+        }
+
+    }
+
+    public void OnUpInput(InputAction.CallbackContext context)
+    {
+
+
+    }
+
+    public void OnDownInput(InputAction.CallbackContext context)
+    {
+
+
+    }
+
+
+    private void OnDestroy()
+    {
+        if (RefManager.shipController == this)
+        {
+            RefManager.shipController = null;
+        }
+        UnSubscibeToInput();
+    }
+
+    public void SubcribeToInput()
+    {
+        InputManager inputManager = RefManager.inputManager;
+        if (inputManager != null)
+        {
+            inputManager.OnMove += OnMoveInput;
+            inputManager.OnUp += OnUpInput;
+            inputManager.OnDown += OnDownInput;
+        }
+    }
+
+    public void UnSubscibeToInput()
+    {
+        InputManager inputManager = RefManager.inputManager;
+        if (inputManager != null)
+        {
+            inputManager.OnMove -= OnMoveInput;
+            inputManager.OnUp -= OnUpInput;
+            inputManager.OnDown -= OnDownInput;
+
+        }
     }
 }
