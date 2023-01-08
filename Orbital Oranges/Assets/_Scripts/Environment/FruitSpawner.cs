96ff4b5b525605def802d47bac2d8123e0d4a70a
chocoland.tv <r.moegenburg@t-online.de>
2023-01-08 17:33:02 +0100

    OutlineShader

---
+++ Orbital Oranges/Assets/_Scripts/Environment/FruitSpawner.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FruitSpawner : MonoBehaviour
+{
+    // STATIC ? 
+    
+    // DEFINE FRUITS AND DATA APPLE ORANGE value1, Banana + Pinapple 2
+    // start void triggers on Opening Cargo
+    // define Random Fruit with value
+    // instantiate in the center of the Cargo 
+    
+    
+}
