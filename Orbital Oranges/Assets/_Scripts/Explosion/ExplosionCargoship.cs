fb77e0edd9a7a505555463d9d72d794cae8fe99f
OliverPohl <oliver@pohl66.de>
2023-01-09 02:51:03 +0100

    yo

--- Orbital Oranges/Assets/_Scripts/Explosion/ExplosionCargoship.cs
+++ Orbital Oranges/Assets/_Scripts/Explosion/ExplosionCargoship.cs
@@ -9,29 +9,48 @@ public class ExplosionCargoship : MonoBehaviour
     [SerializeField] public Material ExplosionRedwMat;
     [SerializeField] public bool explode1 = false, explode2 = false;
     
+
     private GameObject _cargoship;
+
+    private float startEmmission = 0.006f;
+    private float stage2Emmission = 0.05f;
+    private float endEmmission = 1f;
+
+    private float stage1Time = 5f;
+    private float stage2Time = 2.4f;
+    private float explosionTime = 0.1f;
+
+    private float _time;
     
 
     void Start()
     {
         _cargoship = GetComponent<GameObject>();
-        ExplosionYellowMat.SetColor("_EmissionColor", new Color(255,255,0) * 0.006f);
+        ExplosionYellowMat.SetColor("_EmissionColor", new Color(255,255,0) * startEmmission);
     }
 
     void Update()
     {
-        if (explode1)
+        _time += Time.deltaTime;
+        if ((_time > stage1Time) && (_time < (stage1Time + stage2Time)))
         {
             Material[] TempMats = GetComponent<Renderer>().materials;
             TempMats[0] = ExplosionRedwMat;
             TempMats[1] = ExplosionYellowMat;
-
             GetComponent<Renderer>().materials = TempMats;
+
+            // lerp the time here
+            float tmp = Mathf.Lerp(startEmmission, stage2Emmission, stage1Time);
+            ExplosionYellowMat.SetColor("_EmissionColor", new Color(255,255,0) * tmp);
         }
-        if (explode2)
+        else if (_time >= (stage1Time + stage2Time))
         {
+            // lerp the time here and scale fast
             ExplosionYellowMat.SetColor("_EmissionColor", new Color(255,255,0) * 1);
             instableCore.transform.localScale = new Vector3 (0.1f, 0.3f, 0.3f);
+
+
+            // YANNIK we could switch to game over screen with a better camera here
         }
     }
 }
