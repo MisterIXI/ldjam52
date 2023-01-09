207866a1765fbdffd400b319615038adb913ec06
OliverPohl <oliver@pohl66.de>
2023-01-09 15:40:55 +0100

    Settings Updated

--- Orbital Oranges/Assets/_Scripts/GUI/SliderSelect.cs
+++ Orbital Oranges/Assets/_Scripts/GUI/SliderSelect.cs
@@ -2,15 +2,20 @@ using UnityEngine;
 using System.Collections;  
 using UnityEngine.EventSystems;  
 using UnityEngine.UI;
+using TMPro;
 
 
 public class SliderSelect : MonoBehaviour, IPointerEnterHandler, ISelectHandler, IDeselectHandler {
 
+    [SerializeField] public GameObject SliderSelector;
+
+    private TextMeshProUGUI _textSlider;
     private Slider _slider;
-    public GameObject SliderSelector;
+
 
     private void Start() {
         _slider = GetComponent<Slider>();
+        _textSlider = GetComponentInChildren<TextMeshProUGUI>();
     }
 
     public void OnPointerEnter(PointerEventData eventData)
@@ -26,5 +31,12 @@ public class SliderSelect : MonoBehaviour, IPointerEnterHandler, ISelectHandler,
     public void OnDeselect(BaseEventData eventData)
     {
         SliderSelector.SetActive(false);
+    }
+
+
+    private void Update() 
+    {
+        float tmp = Mathf.Round(_slider.value * 100) / 100;
+        _textSlider.text = _slider.value.ToString("F2");
     }
 }
