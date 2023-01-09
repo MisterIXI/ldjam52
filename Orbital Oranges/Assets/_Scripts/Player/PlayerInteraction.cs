f0807f8336c0af7fbfe759c43ce566b81b11e178
Yannik <yannik.braendle@t-online.de>
2023-01-09 17:39:27 +0100

    raycast updates

--- Orbital Oranges/Assets/_Scripts/Player/PlayerInteraction.cs
+++ Orbital Oranges/Assets/_Scripts/Player/PlayerInteraction.cs
@@ -14,7 +14,24 @@ public class PlayerInteraction : MonoBehaviour
         inputManager.OnInteract += FireRay;
     }
 
-
+    private void Update()
+    {
+        RaycastHit hit;
+        int layers = 1 << 3;
+        layers = ~layers;
+        if (Physics.Raycast(transform.position, transform.forward, out hit, interactionDistance, layers))
+        {
+            if (hit.collider.GetComponent<IInteractable>() != null)
+            {
+                IInteractable interactableObject = hit.collider.GetComponent<IInteractable>();
+                interactText.text = interactableObject.GetInteractText();
+            }
+        }
+        else
+        {
+            interactText.text = "";
+        }
+    }
     void FireRay(InputAction.CallbackContext context)
     {
         if (context.performed)
@@ -23,33 +40,29 @@ public class PlayerInteraction : MonoBehaviour
             layers = ~layers;
             Debug.DrawRay(transform.position, transform.forward * interactionDistance, Color.red);
             RaycastHit hit;
-            if(Physics.Raycast(transform.position, transform.forward, out hit, interactionDistance, layers))
+            if (Physics.Raycast(transform.position, transform.forward, out hit, interactionDistance, layers))
             {
 
-                if(hit.collider.GetComponent<IInteractable>() != null)
+                if (hit.collider.GetComponent<IInteractable>() != null)
                 {
                     IInteractable interactableObject = hit.collider.GetComponent<IInteractable>();
                     interactableObject.Interact();
-                    interactText.text = interactableObject.GetInteractText();
                 }
                 else
                 {
                     Rigidbody[] bodies = hit.collider.transform.root.GetComponentsInChildren<Rigidbody>();
-                    foreach(Rigidbody rb in bodies)
+                    foreach (Rigidbody rb in bodies)
                     {
                         rb.drag = 2;
                         rb.angularDrag = 2;
                     }
                 }
-            }   
-            else
-            {
-                interactText.text = "";
             }
         }
     }
 
-    private void OnDestroy() {
+    private void OnDestroy()
+    {
         RefManager.inputManager.OnInteract -= FireRay;
     }
 }
